f72f466b0797e651de4e6a75dbe32ac9de21ca7d
Quazeye <quazeye@shaw.ca>
2020-04-29 19:49:25 -0600

    Working on clouds

--- MountainClimber/Assets/Scripts/CloudMovement.cs
+++ MountainClimber/Assets/Scripts/CloudMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CloudMovement : MonoBehaviour
 {
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,18 @@ public class CloudMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 pos = transform.position;
+        Vector3 viewPos = Camera.main.WorldToViewportPoint(transform.position);
+        if(viewPos.x > -0.1)
+        {
+            Debug.Log(viewPos.x);
+            transform.position += new Vector3(-1 * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(viewPos.x + 6f, viewPos.y, 0);
+        }    
 
-
+        
+        
     }
 }
